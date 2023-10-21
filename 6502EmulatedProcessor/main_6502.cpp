173a81e6f1b8e2f970a867912c65f9dfb6ccf7cc
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-10-21 13:36:31 +0900

    created memory for CPU

--- 6502EmulatedProcessor/main_6502.cpp
+++ 6502EmulatedProcessor/main_6502.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using  u32 = unsigned int;
+
+struct Mem {
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+	void Initialize() {
+		for (u32 i = 0; i < MAX_MEM; i++) {
+			Data[i] = 0;
+		}
+	}
+};
+
 struct CPU {
-	using Byte = unsigned char;
-	using Word = unsigned short;
+
 
 	Word PC; // Program Counter 16 bit
 	Word SP; // Stack Pointer 16 bit
@@ -19,20 +33,22 @@ struct CPU {
 	Byte V : 1; // Overflow Flag
 	Byte N : 1; // Negative Flag
 
-	void Reset() {
+	void Reset(Mem& memory) {
 		PC = 0xFFFC; // Set Program Counter
 		SP = 0x0100; // Set Stack pointer
 		C = Z = I = D = B = V = N = 0; // Clear all bit values
 		A = X = Y = 0; // Reset Registers
+		memory.Initialize(); // Initialize total memory for CPU
 	}
 
 };
 
 int main() {
 
+	Mem mem;
 	CPU cpu;
 
-	cpu.Reset();
+	cpu.Reset(mem);
 
 	return 0;
 }
