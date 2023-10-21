ff43f6cb3458157b96348b7022437668ede77e56
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-10-21 12:08:25 +0900

    added internal struct for CPU data

--- 6502EmulatedProcessor/main_6502.cpp
+++ 6502EmulatedProcessor/main_6502.cpp
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 struct CPU {
+	using Byte = unsigned char;
+	using Word = unsigned short;
+
+	Word PC; // Program Counter 16 bit
+	Word SP; // Stack Pointer 16 bit
+	
+
+	Byte A,X,Y; // Registers 8 bit
+
+	Byte C : 1; // Carry Flag
+	Byte Z : 1; // Zero Flag
+	Byte I : 1; // Interrupt Disable
+	Byte D : 1; // Decimal Mode
+	Byte B : 1; // Break Command
+	Byte V : 1; // Overflow Flag
+	Byte N : 1; // Negative Flag
+
 
 };
 
